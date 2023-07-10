113ce1f08e326d737b580d47f2aca67d7196f54f
Sloane Wolter <sloanewolter@gmail.com>
2023-07-09 21:18:45 -0500

    completed part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I Read this Month";
+   let book1 = "https://m.media-amazon.com/images/I/81XjALHfqkL._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://www.orbitbooks.net/wp-content/uploads/2021/08/Book-of-Gothel-FIN-scaled.jpg";
+   let book3 = "https://img.ws.mms.shopee.ph/ph-11134207-7qul6-libadb4qa1r08b";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Quietly Hostile by Samantha Irby" />
+         <img src={book2} alt="The Book of Gothel by Mary McMyne" />
+         <img src={book3} alt="Fruiting Bodies by Kathryn Harlan" />
       </div>      
    );
 }
