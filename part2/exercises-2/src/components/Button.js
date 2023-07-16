76ef50ab2ce560b62b9591e82a1a345d85034652
Sloane Wolter <sloanewolter@gmail.com>
2023-07-15 22:31:16 -0500

    finished exercises-2

--- part2/exercises-2/src/components/Button.js
+++ part2/exercises-2/src/components/Button.js
@@ -1,9 +1,11 @@
 import './styles.css';
 
 function Button() {
-
+   const onLearnMore = () => {
+      alert("splish splash!")};
+  
    return ( 
-         <button>
+         <button onClick = {onLearnMore}>
             Learn More
          </button>
    );
